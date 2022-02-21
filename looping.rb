d80e1b59e234f5bfe45eabe021bffada1a906a78
Lara <laraellenh@gmail.com>
2022-02-21 10:38:50 -0500

    commit tree 37c147a1c28144f703f2c4cab186c000cd49aa6b

--- looping.rb
+++ looping.rb
@@ -1,6 +1,12 @@
 def happy_new_year
-  # your code here
+  i = 10
+  until i == 0
+    puts i
+    i -= 1
 end
+  puts "Happy New Year!"
+end
+
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
@@ -16,9 +22,18 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  # your code here
+# first parameter/second para in parenthesis separated by two periods . each (method) then do |variable|
+  (1..100).each do |num|
+    puts fizzbuzz(num)
+  end
 end
 
 def reverse_string(str)
-  # your code here
+  #  'hello' to equal "olleh" means we ned to take str and return the letters in reverse, str.length length() is a Array class method which returns the number of elements in the array.  Array.reverse()reverse() is a Array class method which returns a new array containing self’s elements in reverse order. ugh we can't use .reverse...
+  # make reverse string an empty string, then make it = str|s| + reverse_string
+  reverse_string = ""
+  str.length.times do |i|
+    reverse_string = str[i] + reverse_string
+  end
+  reverse_string
 end
